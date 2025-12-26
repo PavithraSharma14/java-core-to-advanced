bf628510c20530ec96d234a2e05b6043d7202a6d
PavithraSharma14 <pavithrasharma456@gmail.com>
2025-12-26 17:28:24 +0530

    add basics programs for comments and user input

---
+++ 01-basics/CommentsDemo.java
@@ -0,0 +1,13 @@
+public class CommentsDemo {
+    // Single-line comment: Explains one line
+
+    /*
+    Multi-line comment:
+    Used when explanation is long
+    */
+
+    public static void main(String[] args) {
+        // This prints text to the screen
+        System.out.println("Learning Comments in Java!");
+    }
+}
