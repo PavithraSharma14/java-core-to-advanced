54f9da2a3fad37b2396368b0a3330c62e563541a
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-01-28 18:58:00 +0530

    feat: add diagonal sum solution avoiding double counting

---
+++ 10-2D-Arrays/practice-questions/solutions/DiagonalSum.java
@@ -0,0 +1,43 @@
+/*
+ * Practice Question: Diagonal Sum of a Square Matrix
+ *
+ * Primary Diagonal  : i == j
+ * Secondary Diagonal: i + j == n - 1
+ *
+ * Time Complexity : O(n^2)
+ * Space Complexity: O(1)
+ */
+
+public class DiagonalSum {
+
+    public static int diagonalSum(int[][] matrix) {
+
+        int n = matrix.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Primary diagonal
+            sum += matrix[i][i];
+
+            // Secondary diagonal (avoid double count)
+            if (i != n - 1 - i) {
+                sum += matrix[i][n - 1 - i];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Diagonal Sum = " + diagonalSum(matrix));
+    }
+}
+
