97a20dc2556055aaf88c7a3e1457637e2335d8bc
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-01-30 19:46:23 +0530

    feat: add basic string input and output example

---
+++ 11-Strings/basics/InputOutput.java
@@ -0,0 +1,27 @@
+package basics;
+/*
+ * Topic: Input and Output with Strings
+ *
+ * Demonstrates how to take string input
+ * and display output in Java.
+ */
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking string input
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        // Displaying output
+        System.out.println("Hello, " + name);
+
+        sc.close();
+    }
+}
+
