69f4de89456519866323e5c26caddce5ecdbbdfa
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-01-30 19:50:18 +0530

    feat: add string length example using length() method

---
+++ 11-Strings/basics/StringLength.java
@@ -0,0 +1,22 @@
+package basics;
+/*
+ * Topic: Length of a String
+ *
+ * The length() method returns the number of characters
+ * present in a string.
+ */
+
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        // Finding length of the string
+        int length = str.length();
+
+        // Printing the length
+        System.out.println("Length of the string: " + length);
+    }
+}
+
