b41e0014ce8bd23c0cbaf6be2c7ee1052d1787dc
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-03-06 21:52:00 +0530

    feat(oops): add BasicClassExample explaining class structure

---
+++ 13-object-oriented-programming/02-classes-and-objects/examples/BasicClassExample.java
@@ -0,0 +1,14 @@
+/**
+ * Example: Basic Class Definition
+ *
+ * A class is a blueprint used to create objects.
+ * It groups related data (fields) and behavior (methods).
+ */
+
+class Student {
+
+    // Fields (variables) that describe a Student object
+    String name;
+    int age;
+
+}
