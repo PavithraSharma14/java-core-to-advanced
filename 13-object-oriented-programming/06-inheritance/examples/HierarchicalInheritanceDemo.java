4c0da4c8765c3ee75cbd3a9308d5609229b80615
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-03-23 21:42:28 +0530

    Fix duplicate class errors by organizing inheritance examples into proper packages

--- 13-object-oriented-programming/06-inheritance/examples/HierarchicalInheritanceDemo.java
+++ 13-object-oriented-programming/06-inheritance/examples/HierarchicalInheritanceDemo.java
@@ -3,19 +3,19 @@
  * Multiple child classes inherit from one parent class
  */
 
-class Animal {
+class Animals {
     void eat() {
         System.out.println("Animal eats");
     }
 }
 
-class Dog extends Animal {
+class Dogs extends Animals {
     void bark() {
         System.out.println("Dog barks");
     }
 }
 
-class Cat extends Animal {
+class Cats extends Animals {
     void meow() {
         System.out.println("Cat meows");
     }
@@ -23,8 +23,8 @@ class Cat extends Animal {
 
 public class HierarchicalInheritanceDemo {
     public static void main(String[] args) {
-        Dog d = new Dog();
-        Cat c = new Cat();
+        Dogs d = new Dogs();
+        Cats c = new Cats();
 
         d.eat();
         d.bark();
