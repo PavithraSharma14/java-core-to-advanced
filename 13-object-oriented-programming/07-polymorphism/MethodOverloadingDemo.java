871941147623785a37ff97e05864d9f3c13b0f5d
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-03-24 19:34:26 +0530

    Example to Implement MethodOverloading polymorphism

---
+++ 13-object-oriented-programming/07-polymorphism/MethodOverloadingDemo.java
@@ -0,0 +1,33 @@
+/**
+ * Method Overloading Example
+ * Same method name, different parameter lists
+ * Decided at compile-time
+ */
+class Calculator {
+
+    // Method with 2 integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with 3 integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method with double values
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class MethodOverloadingDemo {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        // Calling different versions of add()
+        System.out.println(calc.add(2, 3));        // 2 int
+        System.out.println(calc.add(2, 3, 4));     // 3 int
+        System.out.println(calc.add(2.5, 3.5));    // double
+    }
+}
