234170c262c513898bcba8b5fd3d74d24a0fd7db
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-04-19 15:47:25 +0530

    Add example demonstrating importing class from custom package

---
+++ 13-object-oriented-programming/09-packages/examples/import-package/Sum.java
@@ -0,0 +1,8 @@
+import math.Adder;
+
+public class Sum {
+    public static void main(String[] args) {
+        Adder a = new Adder();
+        System.out.println(a.add(5, 7));
+    }
+}
---
+++ 13-object-oriented-programming/09-packages/examples/import-package/math/Adder.java
@@ -0,0 +1,11 @@
+package math;
+
+/*
+ * Class inside 'math' package
+ */
+public class Adder {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
