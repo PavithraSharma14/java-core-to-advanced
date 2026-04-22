a89c0470f567f1fd114ed645c3e7102f7d654784
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-04-22 18:56:34 +0530

    feat(recursion-printing): add sum of n natural numbers using recursion

---
+++ 14-recursion/02-printing-problems/SumOfNNaturalNumbers.java
@@ -0,0 +1,18 @@
+// Find sum of first n natural numbers using recursion
+
+public class SumOfNNaturalNumbers {
+
+    public static int sum(int n) {
+        // Base case
+        if (n == 1) {
+            return 1;
+        }
+
+        // Recursive call
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
