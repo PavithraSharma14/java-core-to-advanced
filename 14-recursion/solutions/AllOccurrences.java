a868ffdd8894c97a23250ab817bcb23d8d22c1b3
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-04-28 16:24:45 +0530

    feat(recursion-solutions): add solution for all occurrences problem

---
+++ 14-recursion/solutions/AllOccurrences.java
@@ -0,0 +1,29 @@
+/*
+PROBLEM:
+Find all indices of a given key in an array using recursion.
+
+APPROACH:
+Traverse array using index.
+If match → print index.
+Continue recursion till end.
+*/
+
+public class AllOccurrences {
+
+    public static void printAll(int[] arr, int key, int i) {
+        if (i == arr.length) {
+            return;
+        }
+
+        if (arr[i] == key) {
+            System.out.print(i + " ");
+        }
+
+        printAll(arr, key, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4, 5, 6, 2, 7, 2, 2};
+        printAll(arr, 2, 0);
+    }
+}
