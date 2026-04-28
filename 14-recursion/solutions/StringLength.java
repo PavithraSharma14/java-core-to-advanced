bdb883bba52afb1400461b43283cf6537f410196
PavithraSharma14 <pavithrasharma456@gmail.com>
2026-04-28 16:36:18 +0530

    feat(recursion-solutions): add string length using recursion

---
+++ 14-recursion/solutions/StringLength.java
@@ -0,0 +1,22 @@
+/*
+PROBLEM:
+Find length of string using recursion.
+
+APPROACH:
+Remove one character each time and count.
+*/
+
+public class StringLength {
+
+    public static int length(String str) {
+        if (str.length() == 0) {
+            return 0;
+        }
+
+        return 1 + length(str.substring(1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(length("hello"));
+    }
+}
